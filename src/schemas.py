118758c2a0960da6fd16cbd410a9514bf126ecde
lealre <renaanvp@gmail.com>
2024-07-29 21:50:35 +0100

    schema: New column added

--- src/schemas.py
+++ src/schemas.py
@@ -9,9 +9,7 @@ class StageSchema(pa.DataFrameModel):
     title: Series[str] = pa.Field(nullable=True)
     price_euro: Series[float] = pa.Field(nullable=True)
     area_m2: Series[float] = pa.Field(nullable=True)
-    num_bedrooms: Series[str] = pa.Field(
-        isin=values_accepted, nullable=True
-    )
+    num_bedrooms: Series[str] = pa.Field(isin=values_accepted, nullable=True)
     property_status: Series[str] = pa.Field(nullable=True)
     location_zone_0: Series[str] = pa.Field(nullable=True)
     location_zone_1: Series[str] = pa.Field(nullable=True)
@@ -19,15 +17,16 @@ class StageSchema(pa.DataFrameModel):
     location_zone_3: Series[str] = pa.Field(nullable=True)
     location_zone_4: Series[str] = pa.Field(nullable=True)
     location_zone_5: Series[str] = pa.Field(nullable=True)
-    location_zone_6: Series[str] = pa.Field(nullable=True) 
-    site: Series[str] = pa.Field(isin = ['sapo'], nullable=True) 
-    link_id: Series[str] = pa.Field(nullable=True)
-    link: Series[str] = pa.Field(nullable= False, unique = True)
+    location_zone_6: Series[str] = pa.Field(nullable=True)
+    is_available: Series[bool] = pa.Field(nullable=False)
+    site: Series[str] = pa.Field(isin=['sapo'], nullable=True)
+    link_id: Series[str] = pa.Field(nullable=False, unique=True)
+    link: Series[str] = pa.Field(nullable=False)
     offer_type_search: Series[str] = pa.Field(nullable=True)
     property_type_search: Series[str] = pa.Field(nullable=True)
     location_search: Series[str] = pa.Field(nullable=True)
     sub_location_search: Series[str] = pa.Field(nullable=True)
-    date_extracted: Series[pa.DateTime] = pa.Field(nullable=False) 
+    date_extracted: Series[pa.DateTime] = pa.Field(nullable=False)
 
     class Config:
         strict = True
