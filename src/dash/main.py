4a635c02377174e3d90f7779871242d714ed357e
lealre <renaanvp@gmail.com>
2024-09-07 00:11:28 +0100

    dash: Proportion treemap added

--- src/dash/main.py
+++ src/dash/main.py
@@ -1,26 +1,40 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, html, dcc
-import plotly.express as px 
+from dash import Dash, html
 
+from src.dash.components import (
+    card_component,
+    heatmap_component,
+    overall_component,
+    proportion_component,
+)
 from src.dash.data import get_data
-from src.dash.components import card_component, overall_component, heatmap_component
 
 df = get_data()
 
-app = Dash(__name__, external_stylesheets=[dbc.themes. DARKLY])
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-app.layout = dbc.Container([
-    html.H1('Lisbon Properties Dash', style={'textAlign':'center', 'padding-bottom': '20px'}),
-    dbc.Row([
-        card_component('Total Ads', df.shape[0]),
-        card_component('Median Price', f'{df.totalPrice.median()} €'),
-        card_component('Median Price / m2', f'{df.pricePerSquareMeter.median()} € / m2'),
-    ]),
-    overall_component(),
-    html.Br(),
-    heatmap_component()
-],
-style={'max-width': '90%'})
+app.layout = dbc.Container(
+    [
+        html.H1(
+            'Lisbon Properties Dash',
+            style={'textAlign': 'center', 'padding-bottom': '20px'},
+        ),
+        dbc.Row([
+            card_component('Total Ads', df.shape[0]),
+            card_component('Median Price', f'{df.totalPrice.median()} €'),
+            card_component(
+                'Median Price / m2',
+                f'{df.pricePerSquareMeter.median()} € / m2',
+            ),
+        ]),
+        overall_component(),
+        html.Br(),
+        proportion_component(),
+        html.Br(),
+        heatmap_component(),
+    ],
+    style={'max-width': '90%'},
+)
 
 
 if __name__ == '__main__':
