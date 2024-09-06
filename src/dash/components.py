a63f0b10b89d8f0ce2d164693355cd54695aefe0
lealre <renaanvp@gmail.com>
2024-09-06 08:35:36 +0100

    dash: Development in progress

--- src/dash/components.py
+++ src/dash/components.py
@@ -1,7 +1,14 @@
+from dash import html, dcc
 import dash_bootstrap_components as dbc
-from dash import html
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
-def get_card_component(title, data):
+from src.dash.data import get_data
+
+df = get_data()
+
+def card_component(title, data):
     component = dbc.Col(
                     dbc.Card(
                         dbc.CardBody([
@@ -14,4 +21,70 @@ def get_card_component(title, data):
                         style={'textAlign': 'center', 'margin-bottom': '20px'}
                     ),
                 )
+    return component
+
+
+def overall_component():
+    color_scale = px.colors.sequential.Viridis 
+    color_map = {f'T{i}': color_scale[i] for i in range(10)}
+    color_map['T9+'] = color_scale[-1]
+
+    fig = px.scatter(
+        df,
+        y = 'totalPrice', 
+        x = 'areaInSquareMeters',
+        color = 'roomsNumberNotation',
+        color_discrete_map=color_map, 
+        # size = 'totalPrice',
+        log_x=True,
+        log_y=True,
+    )    
+    
+    component = dbc.Row([
+        html.H2('Overall Information', style={'padding-bottom': '20px'}),
+        dcc.Graph(figure=fig)
+    ])
+
+    return component
+
+def heatmap_component():
+
+    pivot_df = df.pivot_table(
+        index='location', 
+        columns='roomsNumberNotation', 
+        values='pricePerSquareMeter', 
+        aggfunc='median'
+    )
+
+    pivot_df = pivot_df.apply(pd.to_numeric, errors='coerce')
+    pivot_df = pivot_df.fillna(0)
+
+
+    heatmap = go.Figure(data=go.Heatmap(
+        z=pivot_df.values,
+        x=pivot_df.columns,
+        y=pivot_df.index,
+        colorscale='Blugrn', 
+        colorbar=dict(title='Price per Square Meter'),
+        zmin=pivot_df.values.min(),
+        zmax=pivot_df.values.max(),
+        text = pivot_df.values,
+        texttemplate="%{text:.2f}", 
+        textfont=dict(size=12, color='black'),
+    ))
+
+    heatmap.update_layout(
+        title='Heatmap of Price per Square Meter',
+        xaxis_title='Location',
+        yaxis_title='Rooms Number Notation'
+    )
+
+    component = dbc.Row([
+        html.H2(
+            'Price per m2 - Location x Rooms Number', 
+            style={'padding-bottom': '20px'}
+        ),
+        dcc.Graph(figure=heatmap)
+    ])
+
     return component
--- src/dash/main.py
+++ src/dash/main.py
@@ -3,39 +3,22 @@ from dash import Dash, html, dcc
 import plotly.express as px 
 
 from src.dash.data import get_data
-from src.dash.components import get_card_component
+from src.dash.components import card_component, overall_component, heatmap_component
 
 df = get_data()
-
-color_scale = px.colors.sequential.Viridis  # Use a continuous color scale like 'Viridis'
-color_map = {f'T{i}': color_scale[i] for i in range(10)}
-color_map['T9+'] = color_scale[-1]
-
-fig = px.scatter(
-    df,
-    y = 'totalPrice', 
-    x = 'areaInSquareMeters',
-    color = 'roomsNumberNotation',
-    color_discrete_map=color_map, 
-    # size = 'totalPrice',
-    log_x=True,
-    log_y=True,
-)
-
 
 app = Dash(__name__, external_stylesheets=[dbc.themes. DARKLY])
 
 app.layout = dbc.Container([
     html.H1('Lisbon Properties Dash', style={'textAlign':'center', 'padding-bottom': '20px'}),
     dbc.Row([
-        get_card_component('Total Ads', df.shape[0]),
-        get_card_component('Median Price', f'{df.totalPrice.median()} €'),
-        get_card_component('Median Price / m2', f'{df.pricePerSquareMeter.median()} € / m2'),
+        card_component('Total Ads', df.shape[0]),
+        card_component('Median Price', f'{df.totalPrice.median()} €'),
+        card_component('Median Price / m2', f'{df.pricePerSquareMeter.median()} € / m2'),
     ]),
-    dbc.Row([
-        dcc.Graph(figure=fig)
-    ])
-
+    overall_component(),
+    html.Br(),
+    heatmap_component()
 ],
 style={'max-width': '90%'})
 
